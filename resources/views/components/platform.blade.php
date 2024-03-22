8ed6d0f907b4af602d1d4814fe4a931055c5f950
jesuscam <jesuscamacho.ep@gmail.com>
2024-03-21 22:45:41 -0400

    Ultimo respaldo probado y funcional

--- resources/views/components/platform.blade.php
+++ resources/views/components/platform.blade.php
@@ -12,9 +12,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap-duallistbox@3.0.6/dist/jquery.bootstrap-duallistbox.min.js">
     </script>
-    {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.min.js"
-        integrity="sha512-L0Shl7nXXzIlBSUUPpxrokqq4ojqgZFQczTYlGjzONGTDAcLremjwaWv5A+EDLnxhQzY5xUZPWLOLqYRkY0Cbw=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script> --}}
+
 
     @yield('styles')
 
